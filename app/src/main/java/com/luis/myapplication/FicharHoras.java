b8ad8d8163ec39da71dca3d65a69c07b312b203f
frkolc <frko.lc97@gmail.com>
2023-09-20 18:41:45 +0200

    Añadimos logica a la funcionalidad del ToggleButton donde simplemente mostramos un Toast que al tener la posicion de Entrada indica: "puedes registrar tu jornada laboral" mientras que cuando tiene la vista de salir indica "puedes registrar tu fin de jornada laboral"

--- app/src/main/java/com/luis/myapplication/FicharHoras.java
+++ app/src/main/java/com/luis/myapplication/FicharHoras.java
@@ -1,17 +1,30 @@
 package com.luis.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FicharHoras extends AppCompatActivity {
 
-    Button ficharHoras;
+    ToggleButton boton_toggle_on_off;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fichar_horas);
+
+        boton_toggle_on_off = findViewById(R.id.boton_toggle_on_off);
+        boton_toggle_on_off.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            if (isChecked){
+                Toast.makeText(FicharHoras.this,"Puedes registrar tu jornada laboral", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(FicharHoras.this, "Puedes registrar tu fin de jornada laboral", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        }
     }
-}
