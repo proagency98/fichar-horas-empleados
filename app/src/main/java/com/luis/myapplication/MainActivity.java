9de98f021bb583ec003a42155080de8344b72055
frkolc <frko.lc97@gmail.com>
2023-09-20 18:43:38 +0200

    implementacion de clases

--- app/src/main/java/com/luis/myapplication/MainActivity.java
+++ app/src/main/java/com/luis/myapplication/MainActivity.java
@@ -8,13 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,10 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 iniciarSesionFireBase(email, password);
             }
         });
-
-
-
-
     }
 
     private void iniciarSesionFireBase(String email, String password) {
