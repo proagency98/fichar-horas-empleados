105218b02555e6660af7a92eeca24b7385b90788
frkolc <frko.lc97@gmail.com>
2023-09-20 21:17:23 +0200

    importamos clases para la obtencion de fechas y horas local, tambien realizamos logica para visualizar la fecha actual en un textView y en otro textview la hora actual

--- app/src/main/java/com/luis/myapplication/FicharHoras.java
+++ app/src/main/java/com/luis/myapplication/FicharHoras.java
@@ -2,13 +2,17 @@ package com.luis.myapplication;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FicharHoras extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class FicharHoras extends AppCompatActivity {
     ToggleButton boton_toggle_on_off;
 
     @SuppressLint("MissingInflatedId")
@@ -16,6 +20,19 @@ public class FicharHoras extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fichar_horas);
+
+        TextView txt_FechaActual = findViewById(R.id.txt_FechaActual);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        String fechaActual = sdf.format(new Date());
+        txt_FechaActual.setText("Fecha Actual: " + fechaActual);
+
+        TextView txt_horaActual = findViewById(R.id.txt_horaActual);
+        SimpleDateFormat stz = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        String horaActual = stz.format(new Date());
+        txt_horaActual.setText("Hora Actual: " + horaActual);
+
+
+
 
         boton_toggle_on_off = findViewById(R.id.boton_toggle_on_off);
         boton_toggle_on_off.setOnCheckedChangeListener((compoundButton, isChecked) -> {
