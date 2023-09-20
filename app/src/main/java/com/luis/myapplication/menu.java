0c31e08ec54dd1cabcc3c3c2b3468e5707c23e88
frkolc <frko.lc97@gmail.com>
2023-09-20 18:45:07 +0200

    modificamos la implementacion de la logica para el btn_option_fichar

--- app/src/main/java/com/luis/myapplication/menu.java
+++ app/src/main/java/com/luis/myapplication/menu.java
@@ -1,14 +1,12 @@
 package com.luis.myapplication;
 
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class menu extends AppCompatActivity {
-
     Button btn_optionFichar;
 
     @Override 
@@ -17,14 +15,9 @@ public class menu extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         btn_optionFichar = findViewById(R.id.btn_optionFichar);
-
-        btn_optionFichar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
+        btn_optionFichar.setOnClickListener(view -> {
+            Intent intent = new Intent(menu.this, FicharHoras.class);
+            startActivity(intent);
         });
     }
-
-
 }
