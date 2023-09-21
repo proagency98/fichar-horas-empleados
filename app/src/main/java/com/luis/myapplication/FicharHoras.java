7db086d8ed3d7b139811653515b96141e0c10936
frkolc <frko.lc97@gmail.com>
2023-09-21 19:31:27 +0200

    creamos logica para que podamos observar en pantalla el momento en el que hacemos el registro de las horas en el momento justo en el que tocamos el boton

--- app/src/main/java/com/luis/myapplication/FicharHoras.java
+++ app/src/main/java/com/luis/myapplication/FicharHoras.java
@@ -1,12 +1,12 @@
 package com.luis.myapplication;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,34 +14,47 @@ import java.util.Locale;
 
 public class FicharHoras extends AppCompatActivity {
     ToggleButton boton_toggle_on_off;
+    TextView txt_registro_entrada;
+    TextView txt_registro_salida;
 
-    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fichar_horas);
-
-        TextView txt_FechaActual = findViewById(R.id.txt_FechaActual);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-        String fechaActual = sdf.format(new Date());
-        txt_FechaActual.setText("Fecha Actual: " + fechaActual);
-
-        TextView txt_horaActual = findViewById(R.id.txt_horaActual);
-        SimpleDateFormat stz = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
-        String horaActual = stz.format(new Date());
-        txt_horaActual.setText("Hora Actual: " + horaActual);
-
-
-
-
         boton_toggle_on_off = findViewById(R.id.boton_toggle_on_off);
-        boton_toggle_on_off.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-            if (isChecked){
-                Toast.makeText(FicharHoras.this,"Puedes registrar tu jornada laboral", Toast.LENGTH_LONG).show();
-            }else{
-                Toast.makeText(FicharHoras.this, "Puedes registrar tu fin de jornada laboral", Toast.LENGTH_LONG).show();
+        txt_registro_entrada = findViewById(R.id.txt_registro_entrada);
+        txt_registro_salida = findViewById(R.id.txt_registro_salida);
+
+        boton_toggle_on_off.setOnClickListener(view -> {
+            if (boton_toggle_on_off.isChecked()){
+                registrarSalida();
+                Toast.makeText(FicharHoras.this, "Salida Registrada, buen trabajo!", Toast.LENGTH_LONG).show();
+            } else {
+                registrarEntrada();
+                Toast.makeText(FicharHoras.this, "Entrada Registrada", Toast.LENGTH_LONG).show();
+
             }
         });
-
-        }
     }
+    private void registrarEntrada() {
+        SimpleDateFormat sdfOn = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        String horaEntrada = sdfOn.format(new Date());
+        txt_registro_entrada.setText("Entrada: "+ "\n" + horaEntrada);
+        int colorVerde = ContextCompat.getColor(this, R.color.checked);
+        txt_registro_entrada.setTextColor(colorVerde);
+    }
+    private void registrarSalida() {
+        SimpleDateFormat sdfOff = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        String horaSalida = sdfOff.format(new Date());
+        txt_registro_salida.setText("Salida: "+ "\n" + horaSalida);
+        int colorRojo = ContextCompat.getColor(this, R.color.unChecked);
+        txt_registro_salida.setTextColor(colorRojo);
+    }
+
+
+
+
+}
+
+
