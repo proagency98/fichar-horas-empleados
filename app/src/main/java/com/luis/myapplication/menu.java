4bc354d7fb96687feb420e4d63aa20e439ddb345
proagency98 <proagency98@gmail.com>
2023-10-22 22:13:00 +0200

    Extraemos los datos de FirebaseAuth para que cuando el usuario realice el inicio de sesion se vea el identificador("correo" en este caso) reflejado en el recuadro de bienvenida.

--- app/src/main/java/com/luis/myapplication/menu.java
+++ app/src/main/java/com/luis/myapplication/menu.java
@@ -1,29 +1,52 @@
 package com.luis.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Calendar;
 
 public class menu extends AppCompatActivity {
     ImageButton btn_optionFichar, imgBtn_calendar, imgBtn_documents, imgBtn_solicitud, imgBtn_Exit;
+    TextView txt_bienvenido3;
+    FirebaseAuth mAuth;
 
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+        txt_bienvenido3 = findViewById(R.id.txt_bienvenido3);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        //solicitud de firebase para obtener correo
+        if (user != null){
+           String userName = user.getEmail();
+
+            txt_bienvenido3.setText(userName);
+        }else {
+            txt_bienvenido3.setText("Usuario no autenticado");
+        }
+
+
+
+
         imgBtn_calendar = findViewById(R.id.imgBtn_calendar);
         imgBtn_calendar.setOnClickListener(view -> {
             Intent intent = new Intent(menu.this, Calendar.class);
             startActivity(intent);
         });
-
-
         imgBtn_documents = findViewById(R.id.imgBtn_documents);
         imgBtn_documents.setOnClickListener(view -> {
             Intent intent = new Intent(menu.this, Documentacion.class);
@@ -48,5 +71,15 @@ public class menu extends AppCompatActivity {
             Intent intent = new Intent(menu.this, FicharHoras.class);
             startActivity(intent);
             });
+
+
+
+
+
+
+
+
+
+
     }
 }
